0342d3cc7a6b746e8c4ef0a126da651b75e92922
marksikaundi <emmanuelmark117@gmail.com>
2023-07-22 11:23:27 +0200

    cleanups

---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className=''>
+     <div className=''>logo</div>
+     <div className=''>search</div>
+     <div className=''>list</div>
+    </div>
+  )
+}
+
+export default Header
